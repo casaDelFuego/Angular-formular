ae1d8e15012e3545667524869622e6faf149ed0c
Olga <tselyuk.olga@gmail.com>
2019-04-10 21:26:48 +0200

    form is finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormControl, Validators, AbstractControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, Validators, AbstractControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainFormComponent } from './main-form/main-form.component';
-import { FormerEmployerComponent } from './former-employer/former-employer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MainFormComponent,
-    FormerEmployerComponent
 
   ],
   imports: [
--- src/app/main-form/main-form.component.ts
+++ src/app/main-form/main-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormControl, Validators, AbstractControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, AbstractControl, FormGroup, Validators, FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-main-form',
@@ -7,48 +7,77 @@ import { FormControl, Validators, AbstractControl, FormGroup, ReactiveFormsModul
   styleUrls: ['./main-form.component.css']
 })
 export class MainFormComponent implements OnInit {
+  items: FormArray;
 
-  mainForm: FormGroup = new FormGroup({
-		firstNameControl: new FormControl('', [Validators.required]),
-		lastNameControl: new FormControl('', [Validators.required, Validators.minLength(20), Validators.maxLength(300)]),
-    addressControl: new FormControl('',[Validators.required]),
-    descriptionControl: new FormControl('', [Validators.required]),
-    educationControl: new FormControl('', [Validators.required, Validators.minLength(5)]),
-    graduationYearControl: new FormControl('', [Validators.required])
+
+  mainForm = this.formBuilder.group({
+
+    theName: this.formBuilder.group({
+      firstName: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      lastName: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+    }),
+
+    education: this.formBuilder.group({
+      school: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
+      graduationYear: ['', Validators.compose([Validators.required, Validators.minLength(4)])]
+    }),
+
+    address: this.formBuilder.group({
+      city: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      country: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+    }),
+
+    description: ['', Validators.compose([Validators.required, Validators.minLength(20)])],
+
+    experience: this.formBuilder.array([ this.addExp() ])
+  });
+
+  addExp(): FormGroup  {
+    return this.formBuilder.group({
+      company: ['', Validators.compose([Validators.minLength(3)])],
+      position: ['', Validators.compose([Validators.minLength(3)])],
+      period: ['', Validators.compose([Validators.minLength(3)])]
+    });
   }
-	);
+  get exp(): FormArray {
+    return this.mainForm.get('experience') as FormArray;
+  }
+  onAddExp() {
+    this.items = this.mainForm.get('experience') as FormArray;
+    this.items.push(this.addExp());
+  }
+  onRemoveExp(index: number): void {
+    this.items = this.mainForm.get('experience') as FormArray;
+    this.items.removeAt(index);
+  }
 
-  firstNameControl: AbstractControl;
-  lastNameControl: AbstractControl;
-  addressControl: AbstractControl;
-  descriptionControl: AbstractControl;
-  educationControl: AbstractControl;
-  graduationYearControl: AbstractControl;
+
 
   skills = [
-  {name: 'HTML', selected: false},
-  {name: 'CSS', selected: false},
-  {name: 'JavaScript', selected: false},
-  {name: 'Angular', selected: false},
-  {name: 'UX', selected: false},
-  {name: 'Washing dishes', selected: false},
-  {name: 'Making coffee', selected: false}
-];
-
-  constructor() { }
-
-  ngOnInit() {
-    this.firstNameControl = this.mainForm.controls['firstNameControl'];
-    this.lastNameControl = this.mainForm.controls['lastNameControl'];
-    this.addressControl = this.mainForm.controls['addressControl'];
-    this.descriptionControl = this.mainForm.controls['descriptionControl'];
-    this.educationControl = this.mainForm.controls['educationControl'];
-    this.graduationYearControl = this.mainForm.controls['graduationYearControl'];
-
-  }
+    {name: 'HTML', selected: false},
+    {name: 'CSS', selected: false},
+    {name: 'JavaScript', selected: false},
+    {name: 'Angular', selected: false},
+    {name: 'UX', selected: false},
+    {name: 'Washing dishes', selected: false},
+    {name: 'Making coffee', selected: false}
+    ];
 
   selectSkill(skill) {
     skill.selected = !skill.selected;
   }
+
+
+  onSubmit() {
+    console.log(this.mainForm.value);
+  }
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    console.log(this.mainForm.value);
+  }
+
+
 
 }
